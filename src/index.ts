2b22096d768ad1d725eac4c54c10a03ba50d47a2
Peter Burns <rictic@google.com>
2016-09-23 16:10:57 -0700

    Move to a more structured layout with explicit upgrade passes that can be selected.

--- src/index.ts
+++ src/index.ts
@@ -1,54 +1,82 @@
 /// <reference path="../custom_typings/main.d.ts" />
 
-// import * as clone from 'clone';
+import * as clone from 'clone';
 import * as cssbeautify from 'cssbeautify';
 import * as dom5 from 'dom5';
 import * as parse5 from 'parse5';
 import {Analyzer} from 'polymer-analyzer';
-import {ParsedCssDocument, Visitor} from 'polymer-analyzer/lib/css/css-document';
+import {ParsedCssDocument} from 'polymer-analyzer/lib/css/css-document';
 import {ParsedHtmlDocument} from 'polymer-analyzer/lib/html/html-document';
-import {FSUrlLoader} from 'polymer-analyzer/lib/url-loader/fs-url-loader';
+import {ParsedDocument} from 'polymer-analyzer/lib/parser/document';
 import {Severity, WarningCarryingException} from 'polymer-analyzer/lib/warning/warning';
 import * as shadyCss from 'shady-css-parser';
 
 const p = dom5.predicates;
 
-export async function upgrade(
-    rootFile: string, baseDir: string): Promise<string> {
-  const options = {urlLoader: new FSUrlLoader(baseDir)};
-  const analyzer = new Analyzer(options);
-  const contents = await analyzer.load(rootFile);
-  const rootDoc = await analyzer.analyzeRoot(rootFile);
+export interface Options { upgradePasses: string[]; }
 
-  const parsed = rootDoc.parsedDocument;
-  if (parsed instanceof ParsedCssDocument) {
-    upgradeCss(parsed);
-    return stringifyCss(parsed.ast);
+export class Upgrader {
+  upgradePasses: UpgradePass<any>[];
+
+  constructor(options: Options) {
+    this.upgradePasses =
+        Array.from(new UpgradePassRegistry().getPasses(options.upgradePasses));
   }
-  if (parsed instanceof ParsedHtmlDocument) {
-    upgradeHtml(parsed);
-    return stringifyHtml(parsed.ast, contents);
+
+  async upgrade(rootFile: string, analyzer: Analyzer) {
+    const contents = await analyzer.load(rootFile);
+    const rootDoc = await analyzer.analyzeRoot(rootFile);
+
+    const parsed = clone(rootDoc.parsedDocument);
+    this._upgradeDoc(parsed);
+    return this._stringify(parsed, contents);
   }
-  throw new Error(
-      `Don't yet know how to upgrade a ${rootDoc.parsedDocument.type}`);
+
+  /**
+   * Mutates the given parsed document to upgrade it.
+   */
+  private _upgradeDoc(parsed: ParsedDocument<any, any>) {
+    for (const pass of this.upgradePasses.filter(p => p.canUpgrade(parsed))) {
+      pass.upgrade(parsed);
+    }
+  }
+
+  private _stringify(
+      parsed: ParsedDocument<any, any>, originalContents: string) {
+    if (parsed instanceof ParsedHtmlDocument) {
+      return stringifyHtml(parsed.ast, originalContents);
+    }
+    if (parsed instanceof ParsedCssDocument) {
+      return stringifyCss(parsed.ast);
+    }
+    throw new Error(`Don't yet know how to stringify a ${parsed.type}`);
+  }
 }
 
-function upgradeCss(document: ParsedCssDocument) {
-  document.visit([new ExpressionRewritingVisitor()]);
+const stringifier = new shadyCss.Stringifier();
+function stringifyCss(node: shadyCss.Node): string {
+  return cssbeautify(
+      stringifier.stringify(node),
+      {indent: '  ', autosemicolon: true, openbrace: 'end-of-line'});
 }
 
-function upgradeHtml(document: ParsedHtmlDocument) {
-  const outOfPlaceStyle =
-      p.AND(p.hasTagName('style'), p.parentMatches(p.hasTagName('dom-module')));
-  for (const outOfPlaceNode of dom5.nodeWalkAll(
-           document.ast, outOfPlaceStyle)) {
-    const domModule = outOfPlaceNode.parentNode!;
-    const template =
-        hasExactlyOneChildWithTagName(document, domModule, 'template');
-    dom5.remove(outOfPlaceNode);
-    ((template as any).content as parse5.ASTNode)
-        .childNodes!.unshift(outOfPlaceNode);
+function stringifyHtml(node: parse5.ASTNode, beforeContents: string): string {
+  let result = parse5.serialize(node);
+
+  // Strip out inferred boilerplate nodes that are injected.
+  let m = result.match(/^<html>([^]*)?<\/html>$/);
+  if (m && !beforeContents.includes('<html')) {
+    result = m[1];
   }
+  if (result.startsWith('<head></head>') && !beforeContents.includes('<head')) {
+    result = result.substring('<head></head>'.length);
+  }
+  m = result.match(/^<body>([^]*)?<\/body>$/);
+  if (m && !beforeContents.includes('<body')) {
+    result = m[1];
+  }
+
+  return result;
 }
 
 function hasExactlyOneChildWithTagName(
@@ -76,41 +104,113 @@ function hasExactlyOneChildWithTagName(
   return children[0]!;
 }
 
-function stringifyHtml(node: parse5.ASTNode, beforeContents: string): string {
-  let result = parse5.serialize(node);
 
-  // Strip out inferred boilerplate nodes that are injected.
-  let m = result.match(/^<html>([^]*)?<\/html>$/);
-  if (m && !beforeContents.includes('<html')) {
-    result = m[1];
-  }
-  if (result.startsWith('<head></head>') && !beforeContents.includes('<head')) {
-    result = result.substring('<head></head>'.length);
-  }
-  m = result.match(/^<body>([^]*)?<\/body>$/);
-  if (m && !beforeContents.includes('<body')) {
-    result = m[1];
-  }
+export abstract class UpgradePass<T extends ParsedDocument<any, any>> {
+  code: string;
+  documentClazz: any;
 
-  return result;
+  constructor() {}
+
+  canUpgrade(document: T) { return document instanceof this.documentClazz; }
+
+  abstract upgrade(document: T): void;
 }
 
-const stringifier = new shadyCss.Stringifier();
-function stringifyCss(node: shadyCss.Node): string {
-  return cssbeautify(
-      stringifier.stringify(node),
-      {indent: '  ', autosemicolon: true, openbrace: 'end-of-line'});
+abstract class HtmlUpgradePass extends UpgradePass<ParsedHtmlDocument> {
+  constructor() { super(); }
+  documentClazz = ParsedHtmlDocument;
+}
+abstract class CssUpgradePass extends UpgradePass<ParsedCssDocument> {
+  constructor() { super(); }
+  documentClazz = ParsedCssDocument;
 }
 
-class ExpressionRewritingVisitor implements Visitor {
-  visit(node: shadyCss.Node, _path: shadyCss.Node[]) {
-    if (node.type === 'expression') {
-      const badVarMatcher = /^\s*var\((--[\w-]+)\s*,\s*(--[\w-]+)\)\s*$/;
-      const match = node.text.match(badVarMatcher);
-      if (match) {
-        node.text = `var(${match[1]}, var(${match[2]}))`;
+export class UpgradePassCollection {
+  constructor(public code: string, public passes: string[]) {}
+}
+
+export class UpgradePassRegistry {
+  static singleton: UpgradePassRegistry;
+  private _all = new Map<string, UpgradePass<any>|UpgradePassCollection>();
+  constructor() {
+    if (!UpgradePassRegistry.singleton) {
+      UpgradePassRegistry.singleton = this;
+    }
+
+    return UpgradePassRegistry.singleton;
+  }
+
+  static register(pass: UpgradePass<any>|UpgradePassCollection) {
+    new UpgradePassRegistry()._all.set(pass.code, pass);
+  }
+
+  static get allPassCodes() {
+    const registry = new UpgradePassRegistry();
+    return Array.from(registry._all.values())
+        .filter(p => p instanceof UpgradePass)
+        .map(p => p.code);
+  }
+
+  getPasses(passCodes: string[]): Set<UpgradePass<any>> {
+    const results = new Set<UpgradePass<any>>();
+    for (const code of passCodes) {
+      const passOrCollection = this._all.get(code);
+      if (passOrCollection == null) {
+        throw new Error(`Could not find upgrade pass with code '${code}'`);
+      } else if (passOrCollection instanceof UpgradePassCollection) {
+        for (const pass of this.getPasses(passOrCollection.passes)) {
+          results.add(pass);
+        }
+      } else {
+        results.add(passOrCollection);
       }
-      // console.log(`Expression text: ${node.text}`);
+    }
+    return results;
+  }
+}
+
+class MoveStyleIntoTemplate extends HtmlUpgradePass {
+  code = 'style-into-template';
+
+  constructor() { super(); }
+
+  upgrade(document: ParsedHtmlDocument) {
+    const outOfPlaceStyle = p.AND(
+        p.hasTagName('style'), p.parentMatches(p.hasTagName('dom-module')));
+    for (const outOfPlaceNode of dom5.nodeWalkAll(
+             document.ast, outOfPlaceStyle)) {
+      const domModule = outOfPlaceNode.parentNode!;
+      const template =
+          hasExactlyOneChildWithTagName(document, domModule, 'template');
+      dom5.remove(outOfPlaceNode);
+      ((template as any).content as parse5.ASTNode)
+          .childNodes!.unshift(outOfPlaceNode);
     }
   }
 }
+
+class FixVarStatementWithInvalidCustomPropFallback extends CssUpgradePass {
+  code = 'var-with-invalid-custom-prop-fallback';
+
+  constructor() { super(); }
+
+  upgrade(document: ParsedCssDocument) {
+    document.visit([{
+      visit(node: shadyCss.Node, _path: shadyCss.Node[]) {
+        if (node.type === 'expression') {
+          const badVarMatcher = /^\s*var\((--[\w-]+)\s*,\s*(--[\w-]+)\)\s*$/;
+          const match = node.text.match(badVarMatcher);
+          if (match) {
+            node.text = `var(${match[1]}, var(${match[2]}))`;
+          }
+        }
+      }
+    }]);
+  }
+}
+
+
+
+UpgradePassRegistry.register(new MoveStyleIntoTemplate());
+UpgradePassRegistry.register(
+    new FixVarStatementWithInvalidCustomPropFallback());
--- src/test/index.ts
+++ src/test/index.ts
@@ -3,19 +3,25 @@
 import {assert} from 'chai';
 import * as fs from 'fs';
 import * as path from 'path';
+import {Analyzer} from 'polymer-analyzer';
+import {FSUrlLoader} from 'polymer-analyzer/lib/url-loader/fs-url-loader';
 
-import {upgrade} from '../index';
+import {UpgradePassRegistry, Upgrader} from '../index';
 
 suite('upgrade()', function() {
   const fixturesDir = path.join(__dirname, 'fixtures');
   const dirnames = fs.readdirSync(fixturesDir);
   for (const dirname of dirnames) {
+    const upgrader =
+        new Upgrader({upgradePasses: UpgradePassRegistry.allPassCodes});
     test(`upgrades ${dirname}`, async() => {
       const fullDir = path.join(fixturesDir, dirname);
       const beforeDir = path.join(fullDir, 'before');
+      const analyzer = new Analyzer({urlLoader: new FSUrlLoader(beforeDir)});
+
       for (const file of fs.readdirSync(beforeDir)) {
         const afterPath = path.join(fullDir, 'after', file);
-        const contents = await upgrade(file, beforeDir);
+        const contents = await upgrader.upgrade(file, analyzer);
         assert.deepEqual(contents, fs.readFileSync(afterPath, 'utf-8'));
       }
     });
